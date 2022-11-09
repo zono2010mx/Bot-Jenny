02fa5738a0cef820576712fe12f25221aa428cca
El mishi vaquero <sergiohalo2011@hotmail.com>
2022-11-08 21:04:37 -0500

    Usuarios totales

--- src/index.py
+++ src/index.py
@@ -18,7 +18,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ayuda(ctx):
-    embed = discord.Embed(title=f"{ctx.guild.name}", description="Mira lo que puedo todo lo que puedo hacer :D \n\n **j-ping** - Comprueba que estoy en linea \n **j-saludo** - Te saludo ouo \n **j-suma** *[Numero1]* *[Numero2]* - Sumo dos numeros :o \n **j-ran** *[NumeroMenor]* *[NumeroMayor]* - dame un rango de numeros y te doy uno aleatorio.")
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="Mira lo que puedo todo lo que puedo hacer :D \n\n **j-ping** - Comprueba que estoy en linea \n **j-saludo** - Te saludo ouo \n **j-suma** *[Numero1]* *[Numero2]* - Sumo dos numeros :o \n **j-ran** *[NumeroMenor]* *[NumeroMayor]* - dame un rango de numeros y te doy uno aleatorio. \n **j-miembros** - Te digo el numero de personas en este server.")
     await ctx.send(embed=embed)
 
 @bot.command()
@@ -40,4 +40,10 @@ async def ran(ctx, n1: int, n2: int):
     except ValueError:
         await ctx.send("¡Tu primer numero es mayor que el segundo! ¡Va al reves tontin (￣▽￣*)ゞ")
 
-bot.run('TOKE')
+@bot.command()
+async def miembros(ctx):
+    for guild in bot.guilds:
+       await ctx.send("Somos " + str(len(guild.members)) + " fantasticas personitas en el server")
+       await ctx.send("Obviamente la más importante soy yo o( ❛ᴗ❛ )o")
+
+bot.run('Token')
